c51b87cb9dc045b12cb5e186993fda06013684dc
thomzera <93238590+thomzera@users.noreply.github.com>
2022-05-12 11:21:04 -0300

    Adicionando Recipes na admin do Django

--- ProjetoRecipes/recipes/admin.py
+++ ProjetoRecipes/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 # Register your models here.
 
@@ -9,4 +9,12 @@ class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
 admin.site.register(Category, CategoryAdmin)
+
+# registrei com decorator
+# admin.site.register(Recipe, RecipeAdmin)
